2019b165179e5fbedb44ce837fdc36d75a38aa1f
Fernando-EngComputacao <fernando.furtadocarrilho@gmail.com>
2022-01-10 15:12:51 -0300

    Curso em Video

--- JavaScript/Curso em Video/aula11/ex008.js
+++
@@ -1,3 +0,0 @@
-var velocidade = 60.5
-console.log(`A velocidade do seu carro é ${velocidade} Km/h`)
-console.log(`Dirija sempre usando o sinto de segurança.`)
--- JavaScript/Curso em Video/aula16/funcao01.js
+++
@@ -1,9 +0,0 @@
-function parImpar(n) {
-    if (n % 2 == 0) {
-        return 'Par'
-    } else {
-        return 'Ímpar'
-    }
-}
-
-console.log(parImpar(5))
