94c76216fcd71a013443c8dad57180c828fd8158
Fernando-EngComputacao <fernando.furtadocarrilho@gmail.com>
2022-01-18 18:05:34 -0300

    18-01-2022 JQuery

--- JQuery e Ajax - JS/aula01/script/script.js
+++ JQuery e Ajax - JS/aula01/script/script.js
@@ -1,47 +1,37 @@
+
 function consultaCep(){
 var valor = document.getElementById("txtcep")
 var cep = Number(valor.value)
 var resultado = document.getElementById('resul')
-var vetor = []
-var txt = [ "UF",'Cidade', "Bairro", "Logradouro",  "Complemento", "DDD", "IBGE", "SIAFI","CEP"]
-//vetor = [uf, cidade, bairro, rua, complemento, ddd, ibge, siafi, cep]
-//        [0,     1,     2.     3,       4,       5,   6,    7,     8]
-    
+var p = document.createElement('h4')
+var txt = ``
+resultado.innerHTML = ``
+
     $.ajax({
     url: `http://viacep.com.br/ws/${cep}/json/`,
        type: "GET",
        success: function(response){
-           vetor.push(response.uf)
-           vetor.push(response.localidade)
-           vetor.push(response.bairro)
-           vetor.push(response.logradouro)
-           vetor.push(response.complemento)
-           vetor.push(response.ddd)
-           vetor.push(response.ibge)
-           vetor.push(response.siafi)
-           console.log(vetor)
+           if (!(response.logradouro).length == 0) {
+               txt += `${response.logradouro}, `
+           } 
+           if (!(response.complemento).length == 0){
+               txt += `${response.complemento}, `
+           }
+           if (!(response.bairro).length == 0) {
+               txt += `${response.bairro}, `
+           }
+           if (!(response.localidade).length == 0) {
+               txt += `${response.localidade}`
+           }
+           if (!(response.uf).length == 0) {
+               txt += `-${response.uf}`
+           }
+           if (!(response.cep).length == 0) {
+               txt += ` | CEP: ${response.cep}`
+           }
+           p.innerText = txt
+           resultado.appendChild(p)
+           valor.focus()
         }
     })
-    resultado.innerHTML = ''
-    var tabela = document.createElement('table')
-    var coluna = document.createElement('tr')
-    
-
-    for (let x = 0; x < txt.length; x++) {
-        var linha = document.createElement('td')
-        linha.text = `${txt[x]}` 
-        coluna.appendChild(linha)
-    }
-
-    tabela.appendChild(coluna)
-    coluna = document.createElement('tr')
-    for (let y = 0; y < vetor.length; y++) {
-        var linha = document.createElement('td')
-        linha.text = `${vetor[x]}` 
-        coluna.appendChild(linha)
-        
-    }
-    tabela.appendChild(coluna)
-    resultado.innerHTML = tabela
-
 }
